9043d1cd11354a05e8824f26240fd9693ca8fdd2
Dylan Vines <vinesdn@mail.uc.edu>
2025-02-01 21:42:14 -0500

    Completed Lab_04_04

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        int numToDetermine = 33;
+        int modulo = 2;
+        int remainder = numToDetermine % modulo;
+        System.out.println("The number " + numToDetermine + " has a remainder of " + remainder + " making it an odd number.");
     }
 }
